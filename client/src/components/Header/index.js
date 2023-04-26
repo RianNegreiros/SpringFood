a4e7e96ad9e4bdc2dbf15a972e844da568142b5b
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-04-26 09:06:31 -0300

    add cart modal

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,9 +1,16 @@
+import { useState } from 'react'
 import { Navbar, Nav } from 'react-bootstrap'
 import Image from 'next/image'
 import Link from 'next/link'
 import SearchBox from '@/components/SearchBox'
+import AddressModal from '@/components/AddressModal'
+import CartModal from '@/components/CartModal'
+import { FaCrosshairs, FaShoppingBag } from 'react-icons/fa'
+
 
 export default function Header() {
+  const [ addressModalShow, setAddressModalShow ] = useState(false)
+  const [ cartModalShow, setCartModalShow ] = useState(false)
 
   return (
     <Navbar bg='white' expand='lg' className='border-bottom border-custom-gray'>
@@ -22,11 +29,21 @@ export default function Header() {
       </Navbar.Brand>
 
       <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-
+      
       <Navbar.Collapse id='responsive-navbar-nav' className='justify-content-end'>
+        <Nav className='me-lg-4 me-sm-0 py-2 text-center'>
+          <span className='clickable_effect' onClick={() => setCartModalShow(true)}>
+            <FaShoppingBag className='mb-1'/> Cart
+          </span>
+          <CartModal
+            show={cartModalShow}
+            onHide={() => setCartModalShow(false)}
+            onShow={() => setCartModalShow(true)}
+          />
+        </Nav>
         <Nav className='py-2 text-center'>
           <span className='clickable_effect' onClick={() => setAddressModalShow(true)}>
-            <FaCrosshairs className='mb-1' /> Address
+            <FaCrosshairs className='mb-1'/> Address
           </span>
           <AddressModal
             show={addressModalShow}
@@ -34,7 +51,7 @@ export default function Header() {
             onShow={() => setAddressModalShow(true)}
           />
         </Nav>
-        <SearchBox />
+        <SearchBox/>
       </Navbar.Collapse>
     </Navbar>
   )
