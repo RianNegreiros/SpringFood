ad1530b4901ba389e7960eeee7dd384852040140
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-04-26 07:54:03 -0300

    basic home

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,6 +1,10 @@
 export default function Home() {
   return (
     <>
+      <h1>Find the best restaurants in your neighborhood</h1>
+      <button>
+        <span>Find Now</span>
+      </button>
     </>
   )
 }
