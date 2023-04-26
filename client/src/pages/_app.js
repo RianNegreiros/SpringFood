750bbdcf614cbcb6a275278c28933ffc29d0252a
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-04-26 07:52:47 -0300

    add header

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,5 +1,22 @@
-import '@/styles/globals.css'
+import '@/styles/globals.scss'
+import Head from 'next/head'
+import Header from "@/components/Header"
+import Container from 'react-bootstrap/Container'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Ifood Clone</title>
+        <link ref="icon" href="/favicon.icon" />
+      </Head>
+
+      <main>
+        <Header />
+        <Container className='mt-6'>
+          <Component {...pageProps} />
+        </Container>
+      </main>
+    </>
+  )
 }
