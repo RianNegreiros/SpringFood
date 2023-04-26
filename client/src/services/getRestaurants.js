f2537865c36a3a8fef932ff441edd6994c9a1a4a
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-04-26 08:11:21 -0300

    add search

--- client/src/services/getRestaurants.js
+++ client/src/services/getRestaurants.js
@@ -4,17 +4,17 @@ const fetcher = (...args) => fetch(...args).then((res) => res.json())
 
 export default function getRestaurants() {
   const router = useRouter()
-  const { category } = router.query
+  const { category, q } = router.query
 
   let params = ''
   if (category)
     params = `${params == '' ? '?' : '&'}category=${category}`
+  if (q)
+    params = `${params == '' ? '?' : `${params}&`}q=${q}`
 
   const { data, error } = useSWR(
     `${process.env.apiUrl}/api/restaurants${params}`,
-    fetcher, {
-    revalidateOnFocus: false
-  }
+    fetcher, { revalidateOnFocus: false }
   )
 
   return { restaurants: data, isLoading: !error && !data, isError: error }
