df66053d20e43946df9a4e358d7df5badaeeca1b
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-04-26 08:05:17 -0300

    add category and categories

--- client/src/services/getRestaurants.js
+++ client/src/services/getRestaurants.js
@@ -1,12 +1,20 @@
-import useSWR from 'swr';
+import useSWR from 'swr'
+import { useRouter } from 'next/router'
+const fetcher = (...args) => fetch(...args).then((res) => res.json())
 
 export default function getRestaurants() {
-  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const router = useRouter()
+  const { category } = router.query
+
+  let params = ''
+  if (category)
+    params = `${params == '' ? '?' : '&'}category=${category}`
 
   const { data, error } = useSWR(
-    `${process.env.apiUrl}/api/restaurants`,
-    fetcher,
-    { revalidateOnFocus: false }
+    `${process.env.apiUrl}/api/restaurants${params}`,
+    fetcher, {
+    revalidateOnFocus: false
+  }
   )
 
   return { restaurants: data, isLoading: !error && !data, isError: error }
