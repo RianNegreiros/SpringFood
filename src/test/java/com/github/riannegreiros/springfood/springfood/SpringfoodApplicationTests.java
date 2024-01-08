218ff1fc7461ed1163701ea693374869b31f39a9
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2024-01-08 13:51:34 -0300

    chore(framework): spring boot

---
+++ src/test/java/com/github/riannegreiros/springfood/springfood/SpringfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.riannegreiros.springfood.springfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
